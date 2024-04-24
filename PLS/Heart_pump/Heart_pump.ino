2859d3e3a6d861c827121d7beb437790d42140cd
Rada Berar <ujagaga@gmail.com>
2024-04-24 13:41:28 +0200

    Finished driver code

--- PLS/Heart_pump/Heart_pump.ino
+++ PLS/Heart_pump/Heart_pump.ino
@@ -1,8 +1,11 @@
-// #include "lcdgfx.h"
+#include <ssd1306.h>
 
-#define SERVO1PIN   1
-#define SERVO2PIN   2
-#define PWR_ON_PIN  0
+
+#define SERVO1PIN     1
+#define SERVO2PIN     2
+#define PWR_ON_PIN    0
+#define OLED_SCL_PIN  3
+#define OLED_SDA_PIN  4
 
 /* Servo pulse duration 1000us to 2000us */
 #define MIN_ANGLE   1200
@@ -14,11 +17,16 @@
 #define STATE_UP_EXPAND     2
 #define STATE_DOWN_CONTRACT 3
 #define STATE_DOWN_EXPAND   4
+#define SCREEN_OFFSET       32
+#define IMG_START           40
+#define IMG_TRAIL           70
 
 uint32_t angle1 = MIN_ANGLE;
 uint32_t angle2 = MIN_ANGLE;
 bool pwr_flag = false;
 uint8_t h_state = STATE_OFF;
+uint8_t cursorx = 0;
+uint8_t yoffset;
 
 
 void servo_delay_short(uint32_t duration_us){ 
@@ -42,15 +50,90 @@ void servo_write(int pin, uint32_t duration_us){
   delayMicroseconds(short_pause);
 
 }
+
+void draw_ecg(){
+  uint8_t y;
+  uint8_t x = cursorx & 127; 
+  uint8_t d = (cursorx - IMG_TRAIL) & 127;
+
+  if(x < IMG_START){     
+    yoffset = 0;
+  }else if(x < (IMG_START+2)){
+    yoffset += 2;   
+  }else if(x < (IMG_START+3)){
+    yoffset += 1;   
+  }else if(x < (IMG_START+5)){ 
+
+  }else if(x < (IMG_START+6)){
+    yoffset -= 1;     
+  }else if(x < (IMG_START+8)){
+    yoffset -= 2;  
+  }else if(x < (IMG_START+14)){
+
+  }else if(x < (IMG_START+15)){
+    yoffset -= 3;  
+  }else if(x < (IMG_START+20)){
+    yoffset += 6;  
+  }else if(x < (IMG_START+26)){
+    yoffset -= 6;  
+  }else if(x < (IMG_START+27)){
+    yoffset += 6;  
+  }else if(x < (IMG_START+28)){
+    yoffset += 3;  
+  }else if(x < (IMG_START+32)){ 
+
+  }else if(x < (IMG_START+34)){
+    yoffset += 2;   
+  }else if(x < (IMG_START+35)){
+    yoffset += 1;   
+  }else if(x < (IMG_START+37)){ 
+
+  }else if(x < (IMG_START+38)){
+    yoffset -= 1;     
+  }else if(x < (IMG_START+40)){
+    yoffset -= 2;  
+  }
+
+  y = SCREEN_OFFSET + yoffset;
+  uint8_t clear_buff[16] = {0};
+
+  ssd1306_drawVLine(x, y, y+3);
+  ssd1306_drawBuffer(d, 0, 2, 64, clear_buff);   
+  cursorx++; 
+}
+
+
+
+void draw_screensaver(){
+  ssd1306_fillScreen(0x00); 
+  int x, y, r, dist;
+  r = 16;
+  
+  for (x = 0; x <= r*2; x++){
+    for (y = 0; y <= r * 2; y++){
+      dist = sqrt((r-x) * (r-x) + (r-y) * (r-y));
+    
+      if (dist==r){
+        ssd1306_drawVLine(x+64-r, y+SCREEN_OFFSET-r, y+SCREEN_OFFSET-r+2);
+      }          
+    }   
+    
+  }
+  ssd1306_drawHLine(64-r, SCREEN_OFFSET, 64);
+  ssd1306_drawHLine(64-r, SCREEN_OFFSET+1, 64);
+  ssd1306_drawHLine(64-r, SCREEN_OFFSET-1, 64);
+}
    
 void setup() {
-  // Set up the interrupt that will refresh the servo for us automagically
-  OCR0A = 0xAF;            // any number is OK
   pinMode(PWR_ON_PIN, INPUT); 
   pinMode(SERVO1PIN, OUTPUT); 
   pinMode(SERVO2PIN, OUTPUT); 
   digitalWrite(SERVO1PIN, LOW);
   digitalWrite(SERVO2PIN, LOW); 
+
+  ssd1306_128x64_i2c_initEx(3,4,0);
+  ssd1306_fillScreen(0x00);  
+  draw_screensaver();
 }
 
 void loop()  {
@@ -59,8 +142,10 @@ void loop()  {
   if(h_state == STATE_OFF){
     if(pwr_flag){      
       h_state = STATE_UP_CONTRACT;
+      ssd1306_fillScreen(0x00); 
     }
-  }else{                 
+  }else{  
+    draw_ecg();    
 
     if(h_state == STATE_UP_CONTRACT){
       if(angle1 < MAX_ANGLE){
@@ -91,7 +176,8 @@ void loop()  {
         if(pwr_flag){
           h_state = STATE_UP_CONTRACT;
         }else{
-          h_state = STATE_OFF;          
+          h_state = STATE_OFF;   
+          draw_screensaver();       
         }        
       }
     }
